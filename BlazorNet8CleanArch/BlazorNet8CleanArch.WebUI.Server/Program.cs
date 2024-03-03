c2f39ed78496cb9cd40a557736a47faf98abeeaa
hubert17 <hewbertgabon@gmail.com>
2024-03-03 12:09:32 +0800

    added MudBlazor

--- BlazorNet8CleanArch/BlazorNet8CleanArch.WebUI.Server/Program.cs
+++ BlazorNet8CleanArch/BlazorNet8CleanArch.WebUI.Server/Program.cs
@@ -1,10 +1,17 @@
 using BlazorNet8CleanArch.WebUI.Server.Components;
+using BlazorNet8CleanArch.Application;
+using BlazorNet8CleanArch.Infrastructure;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
