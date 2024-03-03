cbbdece9590130293b005b77b860c17d11788721
hubert17 <hewbertgabon@gmail.com>
2024-03-03 23:41:31 +0800

    Blazor Auto

--- BlazorNet8CleanArch/BlazorNet8CleanArch.WebUI/BlazorNet8CleanArch.WebUI/Program.cs
+++ BlazorNet8CleanArch/BlazorNet8CleanArch.WebUI/BlazorNet8CleanArch.WebUI/Program.cs
@@ -1,22 +1,21 @@
-using BlazorNet8CleanArch.WebUI.Server.Components;
-using BlazorNet8CleanArch.Application;
-using BlazorNet8CleanArch.Infrastructure;
-using MudBlazor.Services;
+using BlazorNet8CleanArch.WebUI.Client.Pages;
+using BlazorNet8CleanArch.WebUI.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure();
-
-builder.Services.AddMudServices();
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -29,6 +28,8 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorNet8CleanArch.WebUI.Client._Imports).Assembly);
 
 app.Run();
