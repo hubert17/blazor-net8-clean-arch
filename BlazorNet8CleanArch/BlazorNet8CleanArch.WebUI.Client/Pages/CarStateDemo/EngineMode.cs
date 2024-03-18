77be96039f8b09721669c23e2062219dfe0ec226
hubert17 <hewbertgabon@gmail.com>
2024-03-19 03:06:29 +0800

    cascadingvalue

---
+++ BlazorNet8CleanArch/BlazorNet8CleanArch.WebUI.Client/Pages/CarStateDemo/EngineMode.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorNet8CleanArch.WebUI.Client.Pages.CarStateDemo
+{
+    public enum EngineMode
+    {
+        Eco,
+        Cruise,
+        Sport
+    }
+
+    public enum ValveTiming
+    {
+        VTEC,
+        VVTI,
+        MIVEC
+    }
+}
